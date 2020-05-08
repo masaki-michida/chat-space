781a269208a27d1db5cc687f9901e4e8bb521d88
masaki-michida <stupid763@gmail.com>
2020-05-08 12:49:03 +0900

    [add]migration file to name column
    [add]email column unique true
    
    [add]pass column unque true

--- db/migrate/20200508033109_devise_create_users.rb
+++ db/migrate/20200508033109_devise_create_users.rb
@@ -4,8 +4,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :name,               null: false, index: true
+      t.string :email,              null: false, unique: true
+      t.string :encrypted_password, null: false, unique: true
 
       ## Recoverable
       t.string   :reset_password_token

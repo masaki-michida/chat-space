f6ba97de6180a908c9bb60a4dd0832d5abbc6927
masaki-michida <stupid763@gmail.com>
2020-05-08 14:17:15 +0900

    [remove] name-index-option [add] add_index unique,index

--- db/migrate/20200508033109_devise_create_users.rb
+++ db/migrate/20200508033109_devise_create_users.rb
@@ -4,9 +4,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :name,               null: false, index: true
-      t.string :email,              null: false, unique: true
-      t.string :encrypted_password, null: false, unique: true
+      t.string :name,               null: false 
+      t.string :email,              null: false, default: 
+      t.string :encrypted_password, null: false, default: 
 
       ## Recoverable
       t.string   :reset_password_token
@@ -37,8 +37,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       t.timestamps null: false
     end
 
+    add_index :users, :name,                 unique: true, index: true
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
+    
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end

f7c9055c717319269f68e5f6a49451369ef37e67
masaki-michida <stupid763@gmail.com>
2020-05-08 14:36:23 +0900

    fix add_index unique index

--- db/migrate/20200508033109_devise_create_users.rb
+++ db/migrate/20200508033109_devise_create_users.rb
@@ -37,7 +37,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       t.timestamps null: false
     end
 
-    add_index :users, :name,                 unique: true, index: true
+    add_index :users, :name,                 unique: true
+    add_index :users, :name,                  index: true
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     
